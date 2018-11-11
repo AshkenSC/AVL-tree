e1bb0c00597ff9fdfdd60f4494331d377ade201b
AshkenSC <393940378@qq.com>
2018-11-11 23:37:21 +0800

    BUPT-1992-2: program clean-up

--- BUPT-1992-2.cpp
+++ BUPT-1992-2.cpp
@@ -12,20 +12,6 @@ BUPT-1992-2
 #include <algorithm>
 using namespace std;
 
-/*
-BUPT-1992-2
-带头结点且头指针为ha和hb的两线性表A和B 分别表示两个集合。
-两表中的元素皆为递增有序。请写一算法求A和B的并集AUB。
-要求该并集中的元素仍保持递增有序。
-且要利用A和B的原有结点空间。
-
-*/
-
-#include <cmath>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
 typedef int ElemType;
 
 typedef struct LNode {
@@ -44,7 +30,6 @@ void unionList(LinkList &l1, LinkList &l2) {
     LNode *p = l1 -> next, *p1 = l1, *q = l2 -> next;
     while(p && q) {
         if(p -> data == q -> data) {
-        //
             p = p -> next;
             q = q -> next;
             p1 = p1 -> next;
